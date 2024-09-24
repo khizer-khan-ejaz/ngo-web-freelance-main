648e95e438f637b7b94b2eceac6df57ed9bb90f2
Khizer Khan Ejaz <khizerkhanejaz7860@gmail.com>
2024-09-24 11:17:33 +0530

    Initial Commit

--- frontend/src/comonents/Blogs.js
+++ frontend/src/comonents/Blogs.js
@@ -1,186 +1,57 @@
-import React, { useContext, useState } from 'react'
-import { Button, Flex, Spinner, Collapse, Text } from '@chakra-ui/react'
-import axios from 'axios'
-import CreateBlog from './CreateBlog';
-import { MdCancel, MdDelete } from "react-icons/md";
-import { Context } from '../../context/Context';
-import DeleteButton from './DeleteButton';
-import useToast from "../../hooks/useToast";
+import React, { useState } from 'react';
+import UseStateHook from '../hooks/UseStateHook';
+import PaginationFooter from './PaginationFooter';
 
 const Blogs = () => {
+    const { data, dataLoading } = UseStateHook(`${process.env.REACT_APP_BACKEND_URL}/api/blogs`);
+    const [page, setPage] = useState(1);
 
-
-  const{showSuccess , showError} = useToast();
-  const{user} = useContext(Context);
-  const token = user.token;
-  const [blogs, setBlogs] = useState([]);
-  const [blogLoading, setBlogLoading] = useState(false);
-  const { setTotalBlogs } = useContext(Context);
-  const [showDelete, setShowDelete] = useState(false);
-
-  useState(() => {
-    (async () => {
-      try {
-
-        setBlogLoading(true);
-
-        const res = await axios({
-          url: `${process.env.REACT_APP_BACKEND_URL}/api/blogs`,
-          method: "get",
-        })
-
-        const data = res.data;
-        console.log(data.blogs)
-        setBlogs(data.blogs);
-        setTotalBlogs(data.blogs.length);
-        localStorage.setItem("totalBlogs", data.blogs.length);
-
-
-      } catch (err) {
-        console.log(err.message);
-      } finally {
-        setBlogLoading(false);
-      }
-
-    })()
-
-  }, [])
-
-  const handleDelete = async (e, blog) => {
-    e.preventDefault();
-    try {
-      const res = await axios({
-        // url: `http://localhost:8080/api/blog/${blog._id}`,
-        url: `/api/blog/${blog._id}`,
-        method: "delete",
-        headers : {
-          'Authorization': `Bearer ${token}`
-        }
-      })
-
-      const data = res.data;
-      if (data.error) {
-        showError(data.error);
-        console.log(data.error);
-        return;
-      }
-
-      showSuccess(data.success);
-      console.log(data);
-      setBlogs(prev => {
-        return prev.filter(item => item._id !== blog._id);
-      })
-
-      setTotalBlogs(prev => {
-        localStorage.setItem("totalBlogs", prev - 1);
-        return prev - 1;
-      })
-
-      setCollapseState(prev => {
-        return data.events.map(item => false);
-      })
-
-
-
-    } catch (err) {
-      console.log(err.message);
+    if (dataLoading) {
+        return <p>Loading...</p>; // Add a loading state or spinner if needed
     }
 
-  }
+    // Check if data and data.blogs are defined
+    if (!data || !data.blogs || data.blogs.length === 0) {
+        return <p>No blogs available.</p>; // Handle the case where no blogs are available
+    }
 
+    return (
+        <>
+            <div>
+                <section className="ftco-section" style={{background:'#FAFBED'  }}>
+                    <div className="container">
+                        <div className="row justify-content-center mb-5 pb-3">
+                            <div className="col-md-7 heading-section text-center">
+                                <h2 className="mb-4">Recent from blog</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                            </div>
+                        </div>
+                        <div className="row d-flex">
+                            {data.blogs.slice(page * 6 - 6, page * 6).map((blog, index) => (
+                                <div key={index} className="col-md-4 d-flex">
+                                    <div className="blog-entry align-self-stretch min-w-[100%]">
+                                        <a href="blog-single.html" className="block-20" style={{ backgroundImage: `url(${blog.img})` }}>
+                                        </a>
+                                        <div className="text p-4 d-block">
+                                            <div className="meta mb-3">
+                                                <div><a href="#"> {blog.date.month} {blog.date.day}, {blog.date.year} </a></div>
+                                                <div><a href="#">Admin</a></div>
+                                                <div><a href="#" className="meta-chat"><span className="icon-chat"></span> 3</a></div>
+                                            </div>
+                                            <h3 className="heading mt-3"><a href="#"> {blog.title} </a></h3>
+                                            <p> {blog.desc} </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </section>
 
-  // for collapse part
-  const [collapseState, setCollapseState] = useState([]);
-
-  const handleToggle = (index) => {
-
-    setCollapseState((prev) => {
-      let newState = [...prev];
-      newState[index] = !newState[index];
-      return newState;
-    })
-
-  }
-
-  if (blogLoading) {
-    return <Flex h={"50px"} justifyContent={"center"} alignItems={"center"} >
-      <Spinner size={"xl"} />
-    </Flex>
-  }
-
-  return (
-    <div className=' scroller w-[100%] p-2 relative h-[100%] gap-[20px]  overflow-scroll '>
-
-
-      {/* delete button */}
-      <div className='flex justify-end w-[100%] transition-all duration-500'>
-        {/* <Button onClick={() => setShowDelete(prev => !prev)} transition={"all 0.5s ease-in-out"} _hover={{ bg: "red.400" }} ml={"auto"} bg={"red"} color={"white"} display={"flex"} gap={"5px"} justifyContent={"center"} alignItems={"center"} >  <Text display={{ base: "none", md: "inline" }} > Delete </Text>  <span> <MdDelete /> </span> </Button> */}
-
-        <DeleteButton setDeleteActivate={setShowDelete} />
-
-      </div>
-
-
-      <div className='flex justify-center transition-all duration-500'>
-        <Flex transition="all 0.3s ease-in-out" w={{ base: "70%", md: "100%" }} mt={"20px"} flexWrap={"wrap"} gap={"20px"} justifyContent={"center"} alignItems={"center"} >
-          {blogs.map((blog, index) => {
-
-            return <div key={index} className='bg-white  transition-all duration-500 w-[100%] flex flex-col justify-center items-center max-w-[300px] shadow-md'>
-
-              {/* delete icon */}
-              <div className={` ${showDelete ? "inline" : "hidden"} w-[100%] flex transition-all duration-500`}>
-                <MdCancel onClick={(e) => handleDelete(e, blog)} cursor={"pointer"} size={"25px"} className='ml-auto transition-all duration-500  hover:text-red-500 ' />
-              </div>
-
-
-              {/* image part */}
-              <div className=' transition-all duration-500  w-[100%]  flex justify-center items-center h-[150px] md:h-[190px] lg:max-h-[220px] object-fill'>
-                <img className='h-[100%] w-[100%]' src={blog.img} alt={blog.img} />
-              </div>
-
-              {/* details part */}
-              <div className='relative transition-all duration-500 -top-[20px] p-[12px]  flex-1 bg-white w-[95%]'>
-
-                <div className='text-gray-400'>
-                  <h1 className='lg:text-[25px]   text-[18px]'>{blog.title}</h1>
-                </div>
-
-                <div className='flex gap-[8px] text-gray-600 '>
-                  <span className='text-gray-600 text-[12px] lg:text-[15px] ' >{blog.date.day}</span>
-                  <span className='text-gray-600 text-[12px] lg:text-[15px] ' >{blog.date.month}</span>
-                  <span className='text-gray-600 text-[12px] lg:text-[15px] ' >{blog.date.year}</span>
-                </div>
-
-
-
-                <div className={` scroller overflow-scroll ${collapseState[index] ? "h-auto" : "max-h-[100px]"} `}>
-
-                  {/* collapse feature */}
-                  <Collapse startingHeight={20} in={collapseState[index]} >
-                    <p className='text-[14px] md:text-[16px]'> {blog.desc} </p>
-                  </Collapse>
-                  <Button size="sm" onClick={() => handleToggle(index)} mt="1rem">
-                    Show {collapseState[index] ? "Less" : "More"}
-                  </Button>
-
-                </div>
-
-
-              </div>
-
-
-
+            
             </div>
+        </>
+    );
+};
 
-          })}
-
-        </Flex>
-      </div>
-
-      <CreateBlog blogs={blogs} setBlogs={setBlogs} setTotalBlogs={setTotalBlogs} />
-
-    </div>
-  )
-}
-
-export default Blogs
+export default Blogs;
