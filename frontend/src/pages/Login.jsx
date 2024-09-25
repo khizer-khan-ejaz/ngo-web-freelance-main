25252c7e9ecc1318091c09fa9dfb34672c450a84
Khizer Khan Ejaz <khizerkhanejaz7860@gmail.com>
2024-09-25 20:58:48 +0530

    Forcing file updates

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -92,7 +92,7 @@ function Login() {
         try {
             setLoginLoading(true);
             const res = await axios({
-                url: "http://http://3.110.255.102:8080/api/admin/login",
+                url: "http://3.110.255.102:8080/api/admin/login",
                 method: "post",
                 data: { email: loginDetails.email, password: loginDetails.password },
                 headers: {
